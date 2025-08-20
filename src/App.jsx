f66ac11ed9b0962fd76dcd537c7d2b9ffc7693b6
Duong Nam <dnam281224@gmail.com>
2025-08-20 15:37:43 +0700

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Dropdown } from 'react-bootstrap'
+import { useState } from "react";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
